331b904775d56bb22492132b23b23aa98d9f81c8
Maksim Kalinin <maksim@MacBook-Pro-Maksim-2.local>
2019-09-08 12:53:59 +0300

    bugs

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import { writable } from 'svelte/store';
 
 export const userType = writable('user');
 
-export function setUserType (newUserType) {
+export function setUserType(newUserType) {
   console.log('set userType');
-  (newUserType === 'user' || newUserType === 'admin') && userType.set(newUserType);
-};
+  (newUserType === 'user' || newUserType === 'admin') &&
+    userType.set(newUserType);
+}
